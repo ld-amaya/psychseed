33da67f7fa384dbf7265a7eb8474945db26cb7c9
lou <louamaya@me.com>
2020-10-13 01:50:06 -0700

    add index.html

--- app.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask, redirect
-
-app = Flask(__name__)
-
-
-@app.route("/")
-def redirect_page():
-    """Redirect to Facebook Page"""
-    return redirect("https://www.facebook.com/iwillpsychseed")
